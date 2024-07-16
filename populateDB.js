e2ae2bea0a99e6df886195bd27ef632ce82421b0
adarsaparasar <adarsaparasarbehera@gmail.com>
2024-07-16 11:19:27 +0530

    First commit
    FC

---
+++ populateDB.js
@@ -0,0 +1,63 @@
+
+const mongoose = require('mongoose');
+const Dish = require('./dishmodels');
+
+const sampleData = [
+    {
+        "dishName": "Jeera Rice",
+        "dishId": "1",
+        "imageUrl": "https://nosh-assignment.s3.ap-south-1.amazonaws.com/jeera-rice.jpg",
+        "isPublished": true
+    },
+    {
+        "dishName": "Paneer Tikka",
+        "dishId": "2",
+        "imageUrl": "https://nosh-assignment.s3.ap-south-1.amazonaws.com/paneer-tikka.jpg",
+        "isPublished": true
+    },
+    {
+        "dishName": "Rabdi",
+        "dishId": "3",
+        "imageUrl": "https://nosh-assignment.s3.ap-south-1.amazonaws.com/rabdi.jpg",
+        "isPublished": true
+    },
+    {
+        "dishName": "Chicken Biryani",
+        "dishId": "4",
+        "imageUrl": "https://nosh-assignment.s3.ap-south-1.amazonaws.com/chicken-biryani.jpg",
+        "isPublished": true
+    },
+    {
+        "dishName": "Alfredo Pasta",
+        "dishId": "5",
+        "imageUrl": "https://nosh-assignment.s3.ap-south-1.amazonaws.com/alfredo-pasta.jpg",
+        "isPublished": true
+    }
+];
+
+
+
+const url = 'mongodb+srv://mongo:3Yg7zWTaCmTb1Ots@cluster0.slsdvon.mongodb.net/DishesDB';
+
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    const promises = sampleData.map(dishData => {
+      const dish = new Dish(dishData);
+      return dish.save().then(savedDish => {
+        console.log(`Saved: ${savedDish.dishName}`);
+      }).catch(error => {
+        console.error(`Error saving ${dishData.dishName}:`, error);
+      });
+    });
+
+    return Promise.all(promises);
+  })
+  .then(() => {
+    console.log('All dishes saved');
+    mongoose.connection.close();
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+  });
---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
+const Dish = require('./dishmodels');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const port = 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(cors());
+const url = 'mongodb+srv://mongo:3Yg7zWTaCmTb1Ots@cluster0.slsdvon.mongodb.net/DishesDB';
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+
+    io.on('connection', (socket) => {
+      console.log('a user connected');
+    });
+
+    // Fetch all dishes
+    app.get('/api/dishes', async (req, res) => {
+      try {
+        const dishes = await Dish.find({});
+        res.json(dishes);
+      } catch (error) {
+        res.status(500).json({ error: 'Error fetching dishes' });
+      }
+    });
+
+    // Toggle the published status
+    app.put('/api/dishes/:dishId/toggle', async (req, res) => {
+      const { dishId } = req.params;
+      try {
+        const dish = await Dish.findOne({ dishId });
+        if (dish) {
+          dish.isPublished = !dish.isPublished;
+          await dish.save();
+          io.emit('dishUpdated', dish);
+          res.json(dish);
+        } else {
+          res.status(404).send('Dish not found');
+        }
+      } catch (error) {
+        res.status(500).json({ error: 'Error toggling dish status' });
+      }
+    });
+
+    server.listen(port, () => {
+      console.log(`Server running on http://localhost:${port}`);
+    });
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+  });
